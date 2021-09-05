324907fc01ea19d52d0f7b546820f619ef010222
Alexander Petrovskiy <apetrovskiy2@hotmail.com>
2021-09-05 18:14:02 +0300

    further changes

--- Solidity/rinkeby_demo/test/Zazzle.test.js
+++ Solidity/rinkeby_demo/test/Zazzle.test.js
@@ -3,7 +3,11 @@ const Zazzle = artifacts.require("Zazzle")
 contract("Zazzle", (accounts) => {
     console.log(accounts)
 
-    BaforeUnloadEvent(async() => {
+    before(async() => {
         zazzle = await Zazzle.deployed()
+    })
+
+    it("gives the owner of the token 1M tokens", async() => {
+        let balance = zazzle.balanceOf(accounts[0])
     })
 })
